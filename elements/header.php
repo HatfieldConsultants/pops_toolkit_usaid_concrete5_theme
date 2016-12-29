5da7d0d18c0045f3928e76e36d0bd712989f360f
Adam Soltys <asoltys@gmail.com>
2016-12-29 09:14:50 -0800

    Change header

--- elements/header.php
+++ elements/header.php
@@ -7,30 +7,23 @@ $blocks = $as->getTotalBlocksInArea();
 $displayThirdColumn = $blocks > 0 || $c->isEditMode();
 ?>
 
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4 col-xs-6">
-                <?php
-                $a = new GlobalArea('Header Site Title');
-                $a->display();
-                ?>
-            </div>
-            <div class="<?php echo ($displayThirdColumn) ? 'col-sm-5 col-xs-6' : 'col-sm-8 col-xs-6' ?>">
-                <?php
-                $a = new GlobalArea('Header Navigation');
-                $a->display();
-                ?>
-            </div>
-            <?php
-            if ($displayThirdColumn) {
-                ?>
-                <div class="col-sm-3 col-xs-12">
-                    <?php $as->display(); ?>
-                </div>
-                <?php
-            }
-            ?>
-        </div>
+<header class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <a href="#/home" class="">
+        <img src="<?php echo $this->getThemePath(); ?>/images/logo.png" width="200px" style="margin-bottom: 15px;">
+      </a>
+    </div>
+    <div class="col-md-6 text-right USAIDLogo">
+      <a href="https://www.usaid.gov">
+        <img src="https://www.usaid.gov/sites/all/themes/usaid/logo.png" alt="U.S. Agency for International Development">
+      </a>
+    </div>
+  </div>
+    <div class="row">
+      <?php
+      $a = new GlobalArea('Header Navigation');
+      $a->display();
+      ?>
     </div>
 </header>
